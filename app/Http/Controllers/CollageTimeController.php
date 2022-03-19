816bdb35972d78fb88466079491693ef4f5a5309
Jamal Alawadi <eng.jamal1999@gmail.com>
2022-03-19 23:18:14 +0200

    Dashboard

---
+++ app/Http/Controllers/CollageTimeController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Collage_time;
+use Illuminate\Http\Request;
+
+class CollageTimeController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Collage_time  $collage_time
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Collage_time $collage_time)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Collage_time  $collage_time
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Collage_time $collage_time)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Collage_time  $collage_time
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Collage_time $collage_time)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Collage_time  $collage_time
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Collage_time $collage_time)
+    {
+        //
+    }
+}
